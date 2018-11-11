7593e247555a4ef98c87c744023f2ef5cbb71857
saikishan <saikishan2008@gmail.com>
2018-11-11 22:46:40 +0530

    codechef is also configured and working well when tested

--- codechef.py
+++ codechef.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import urllib.request
+from urllib.request import urlopen, Request
 import threading
 import time
 import random
@@ -7,56 +7,61 @@ import random
 class Codechef:
     def get_count_of_pages(self, url):
         print("page-count url" + url)
-        content = urllib.request.urlopen(url).read()
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        content = urlopen(req).read()
         soup  = BeautifulSoup(content,features="html.parser")
-        page_count = int(soup.body.find(class_="page-item last-page", recursive=True).a.get("data-attr8"))
+        line = soup.body.find(class_="pageinfo", recursive=True).text
+        number = line[line.find('f')+1:]
+        page_count = int(number)
         return page_count
 
     def get_username(self, row):
         try:
-            name = row.find(recursive=True, attrs = {"data-analytics" : "LeaderboardListUserName"}).text
-            rank = int(row.find(recursive=True, class_ = "table-row-column ellipsis rank" ).div.text)
+            name = row.a.get("title")
         except AttributeError as ae:
             print(ae)
             return None
-        if rank == 1:
-            return name
-        return None
+        return name
 
     def scan_the_page(self, url, student_dict, page_id):
         print("in page no " + str(page_id))
-        current_url = url + "?limit=100&page={}".format(page_id)
+        current_url = url + "?page={}&sort_by=All&sorting_order=asc&language=All&status=15&handle=&Submit=GO".format(page_id)
         content = None
         while content is None:
             try:
-                content = urllib.request.urlopen(current_url).read()
+                req = Request(current_url, headers={'User-Agent': 'Mozilla/5.0'})
+                content = urlopen(req).read()
             except Exception as e:
                 print(e)
         soup = BeautifulSoup(content, features="html.parser")
-        user_table = soup.findAll(class_="table-row-wrapper", recursive=True)
-        for row in user_table:
+        rows  = soup.body.findAll(recursive=True, attrs = {"width" : "144"})    
+        for row in rows:
             name = self.get_username(row)
             print(name)
             if name and name in student_dict:
                 student_dict[name] = True
 
     def thread_caller(self, url, page_start, page_end, student_dict):
-        print("strart page ", page_start, "end page" , page_end )
+        print("strat page ", page_start, "end page" , page_end )
         for x in range(page_start, page_end):
             self.scan_the_page(url, student_dict, x)
 
     def get_filtered_list_from_webpage(self, url, student_list):
-        initial_page_url = url + "?limit=100&page=1"
+        initial_page_url = url + "?page={}&sort_by=All&sorting_order=asc&language=All&status=15&handle=&Submit=GO".format(0)
         pagecount = self.get_count_of_pages(initial_page_url)
         student_dict = { x:False for x in student_list}
         print("page_count", pagecount)
+
         def divide_to_sets(pagecount,no_of_threads):
             share = pagecount // no_of_threads
+            if share < 1:
+                yield(1,pagecount)
+                return
             yield (0, 1*share)
             for x in range(1,no_of_threads-1):
                 yield (x*share,(x+1)*share)
             yield((no_of_threads-1)*share,pagecount+1)
-
+        
         thread_list = []
         for x in divide_to_sets(pagecount, 8):
             thread = threading.Thread(target=self.thread_caller,args=(url,x[0],x[1],student_dict))
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from hackerrank import Hackerrank
-
+from codechef import Codechef
 #this is to get all table records >>> body.findAll(class_="table-row-wrapper", recursive=True)
-hr_scraper = Hackerrank()
-Hacker_rank_student_l = ['15PA1A05E0']
-url = "https://www.hackerrank.com/challenges/a-very-big-sum/leaderboard"
-stu_dict = hr_scraper.get_filtered_list_from_webpage(url, student_l)
+def hackerrank_testing():
+    hr_scraper = Hackerrank()
+    hackerrank_student_l = ['15PA1A05E0']
+    h_url = "https://www.hackerrank.com/challenges/a-very-big-sum/leaderboard"
+    stu_dict = hr_scraper.get_filtered_list_from_webpage(h_url, hackerrank_student_l)
+    return stu_dict
+
+def codechef_testing():
+    cc_scraper = Codechef()
+    c_url = "https://www.codechef.com/status/CHSERVE"
+    codechef_student_l = ['15PA1A05E0']
+    stu_dict = cc_scraper.get_filtered_list_from_webpage(c_url , codechef_student_l)
+    return stu_dict
+
+print(codechef_testing())
